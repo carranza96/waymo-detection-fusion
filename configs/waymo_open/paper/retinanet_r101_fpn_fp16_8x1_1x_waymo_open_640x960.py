7c879a017e3b2c09ece9ccea720bc21dcb478668
manuel <manuelcarranza96@gmail.com>
2020-12-08 12:37:57 +0100

    New pipelines

---
+++ configs/waymo_open/paper/retinanet_r101_fpn_fp16_8x1_1x_waymo_open_640x960.py
@@ -0,0 +1,27 @@
+_base_ = [
+    '../../_base_/models/retinanet_r50_fpn.cpy',
+    '../../_base_/datasets/waymo_detection_640x960.py',
+    '../../_base_/schedules/schedule_1x.py', '../../_base_/default_runtime.py'
+]
+
+model = dict(pretrained='torchvision://resnet101',
+             backbone=dict(depth=101),
+             bbox_head=dict(num_classes=3))
+
+data = dict(samples_per_gpu=8)
+
+
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[2])
+total_epochs = 3
+
+load_from = 'http://download.openmmlab.com/mmdetection/v2.0/retinanet/retinanet_r101_fpn_2x_coco/retinanet_r101_fpn_2x_coco_20200131-5560aee8.pth'
+# fp16 settings
+fp16 = dict(loss_scale=512.)

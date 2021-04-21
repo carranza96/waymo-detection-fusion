b8d11ccb3a15e69bda6b9177d62db313f290bd86
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2021-04-22 01:10:29 +0200

    Update config

--- configs/waymo_pablo/faster_rcnn_r50_fpn_fp16_8x1_1x_waymo_open_1280x1920.py
+++ configs/waymo_pablo/faster_rcnn_r50_fpn_fp16_8x1_1x_waymo_open_1280x1920.py
@@ -12,17 +12,18 @@ model = dict(roi_head=dict(bbox_head=dict(num_classes=3)))
 data = dict(samples_per_gpu=2)
 
 # LR is set for a batch size of 8
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 
-# learning policy
+# Learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=0.001,
-    step=[8, 11])
-total_epochs = 12
+    step=[3, 4])
+total_epochs = 6
+runner = dict(type='EpochBasedRunner', max_epochs=6)
 
 load_from = 'https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth'  # noqa
 #load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa

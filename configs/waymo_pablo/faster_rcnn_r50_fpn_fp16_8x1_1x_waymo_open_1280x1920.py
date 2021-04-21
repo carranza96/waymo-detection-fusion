2261529cb9c36ef708b2d05ce8b868a71f857bdf
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2021-04-21 23:44:54 +0200

    Reduce batch

--- configs/waymo_pablo/faster_rcnn_r50_fpn_fp16_8x1_1x_waymo_open_1280x1920.py
+++ configs/waymo_pablo/faster_rcnn_r50_fpn_fp16_8x1_1x_waymo_open_1280x1920.py
@@ -5,15 +5,16 @@ _base_ = [
     '../_base_/default_runtime.py'
 ]
 
-# model
+# Model
 model = dict(roi_head=dict(bbox_head=dict(num_classes=3)))
-# data
-data = dict(samples_per_gpu=4)
 
+# Data
+data = dict(samples_per_gpu=2)
 
-# lr is set for a batch size of 8
+# LR is set for a batch size of 8
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
+
 # learning policy
 lr_config = dict(
     policy='step',
@@ -24,6 +25,7 @@ lr_config = dict(
 total_epochs = 12
 
 load_from = 'https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth'  # noqa
+#load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'  # noqa
 
 # fp16 settings
 fp16 = dict(loss_scale=512.)

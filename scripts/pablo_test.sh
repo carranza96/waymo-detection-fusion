c35a8cb07e2020439da1ef51a43b4ce47e43e28c
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2021-05-04 21:32:58 +0200

    Add custom test scripts

--- scripts/pablo_test.sh
+++ scripts/pablo_test.sh
@@ -1,11 +1,12 @@
 MODEL=faster_rcnn_r50_fpn_fp16_8x1_1x_waymo_open_1280x1920
+DIR=saved_models/modelo-normal/
 
-python3 tools/test.py configs/waymo_pablo/${MODEL}.py \
-    saved_models/waymo_pablo/${MODEL}.pth \
-    --show \
+
+python tools/test.py ${DIR}/${MODEL}/${MODEL}.py \
+    ${DIR}/${MODEL}/latest.pth \
     --eval bbox \
-#    --out saved_models/study/${MODEL}/results.pkl  \
-#    --eval-options "classwise=True" "outfile_prefix=saved_models/"${MODEL}/"predictions_waymo" \
+    --out ${DIR}/${MODEL}/results.pkl  \
+    --eval-options "classwise=True" "outfile_prefix=saved_models/"${MODEL}/"predictions_waymo" \
 #    --show-dir saved_models/${MODEL}/predictions   # Save images with predictions
 #    --format-only \
 #    --eval-options "outfile_prefix=./faster_rcnn_res" "format_type=waymo"

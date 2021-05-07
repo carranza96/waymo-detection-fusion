18b1d66ccc0e68211c31914efa22d7926668451d
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2021-05-08 01:23:46 +0200

    Fix visualize

--- scripts/pablo_visualize.sh
+++ scripts/pablo_visualize.sh
@@ -1,8 +1,8 @@
 MODEL=faster_rcnn_r50_fpn_fp16_8x1_1x_waymo_open_1280x1920
-DIR=saved_models/modelo-normal/
+DIR=saved_models/modelo-normal
 
 python DetVisGUI/DetVisGUI.py \
 ${DIR}/${MODEL}/${MODEL}.py \
---det_file ${DIR}/${MODEL}/${MODEL}/results.pkl
+--det_file ${DIR}/${MODEL}/results.pkl
 
 # [--stage ${STAGE}] [--output ${SAVE_DIRECTORY}]

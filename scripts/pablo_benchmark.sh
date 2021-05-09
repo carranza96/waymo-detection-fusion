5d4230c3be86c86dfa481c624c32c95af794bb7a
Pablo-Davila <Pablo-Davila@users.noreply.github.com>
2021-05-09 18:33:24 +0200

    Add usual config options to scripts

--- scripts/pablo_benchmark.sh
+++ scripts/pablo_benchmark.sh
@@ -1,4 +1,9 @@
 MODEL=faster_rcnn_r50_fpn_fp16_2x1_6e_waymo_open_1280x1920
+#MODEL=faster_rcnn_r50_c4_fp16_2x1_6e_waymo_open_1280x1920
+
+CHECKPOINT_DIR=FRCNN-FPN-normal_001
+#CHECKPOINT_DIR=FRCNN-FPN-learn-anchors_002
+#CHECKPOINT_DIR=FRCNN-C4-normal_001
 
 python tools/analysis_tools/benchmark.py configs/waymo_pablo/${MODEL}.py \
-       saved_models/modelo-normal/${MODEL}/latest.pth
+       saved_models/${CHECKPOINT_DIR}/${MODEL}/latest.pth

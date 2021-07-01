304465421365913d17cd14b080437d5bb120ec1b
carranza96 <manuelcarranza96@gmail.com>
2021-07-01 10:57:15 +0200

    Added new configs

--- scripts/eval_metric.sh
+++ scripts/eval_metric.sh
@@ -1,4 +1,4 @@
-MODEL=study/yolof_r50_c5_fp16_4x2_1x_1280x1920
+MODEL=study/faster_rcnn_r50_fpn_fp16_4x2_1x_1280x1920
 
 
 # Format-only
@@ -8,23 +8,23 @@ MODEL=study/yolof_r50_c5_fp16_4x2_1x_1280x1920
 
 
 # Eval
-#python tools/analysis_tools/eval_metric.py configs/waymo_open/${MODEL}.py \
-#saved_models/${MODEL}/results.pkl \
-#--eval bbox \
-#--eval-options "classwise=True" "outfile_prefix=saved_models/"${MODEL}/"predictions_waymo"
+python tools/analysis_tools/eval_metric.py configs/waymo_open/${MODEL}.py \
+saved_models/${MODEL}/results.pkl \
+--eval bbox \
+--eval-options "classwise=True" "outfile_prefix=saved_models/"${MODEL}/"predictions_waymo"
 #
 #
 #
-PREDICTIONS_FILE=saved_models/${MODEL}/predictions_waymo_full_val.bin
-GTS_FILE=saved_models/${MODEL}/predictions_waymo_full_val_gt.bin
-METRICS_FILE=saved_models/${MODEL}/metrics.csv
+#PREDICTIONS_FILE=saved_models/${MODEL}/predictions_waymo_full_val.bin
+#GTS_FILE=saved_models/${MODEL}/predictions_waymo_full_val_gt.bin
+#METRICS_FILE=saved_models/${MODEL}/metrics.csv
+##
+#cd waymo-open-dataset
+#bazel build waymo_open_dataset/metrics/tools/compute_detection_metrics_main
+#cd ..
 #
-cd waymo-open-dataset
-bazel build waymo_open_dataset/metrics/tools/compute_detection_metrics_main
-cd ..
-
-waymo-open-dataset/bazel-bin/waymo_open_dataset/metrics/tools/compute_detection_metrics_main \
-${PREDICTIONS_FILE} ${GTS_FILE} > ${METRICS_FILE}
-
-
-python tools/parse_waymo_metrics.py --metrics_file=${METRICS_FILE}
+#waymo-open-dataset/bazel-bin/waymo_open_dataset/metrics/tools/compute_detection_metrics_main \
+#${PREDICTIONS_FILE} ${GTS_FILE} > ${METRICS_FILE}
+#
+#
+#python tools/parse_waymo_metrics.py --metrics_file=${METRICS_FILE}
